f7aefe7b34aaf13e71675f406cb77743a45399ba
Jean-Sebastien Morel <js.morel130@gmail.com>
2021-08-26 17:42:58 +0200

    booking model controller front-end

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,14 +1,16 @@
 class BookingsController < ApplicationController
   def new
-    authorize @booking
     @piece = Piece.find(params[:piece_id])
     @booking = Booking.new
+    authorize @piece
   end
 
   def create
     @booking = Booking.new(booking_params)
     @piece = Piece.find(params[:piece_id])
     @booking.piece = @piece
+    @booking.user = current_user
+    authorize @piece
     @booking.save
     redirect_to piece_path(@piece)
   end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,6 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :piece
   has_one :review
+  validates :date_start, presence: true
+  validates :date_end, presence: true
 end
