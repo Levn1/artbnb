7499b0b147ce49fba7058b9d5b1b2d9f002654c9
luis ghidelli <luisfernando.ghidelli@gmail.com>
2021-08-26 16:12:18 +0200

    booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,3 +1,21 @@
 class BookingsController < ApplicationController
+  def new
+    authorize @booking
+    @piece = Piece.find(params[:piece_id])
+    @booking = Booking.new
+  end
 
+  def create
+    @booking = Booking.new(booking_params)
+    @piece = Piece.find(params[:piece_id])
+    @booking.piece = @piece
+    @booking.save
+    redirect_to piece_path(@piece)
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:content)
+  end
 end
